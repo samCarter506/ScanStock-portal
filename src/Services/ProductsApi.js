2b749d6151d7cc35290735d0936b4c74dd2468f1
thamaesamuel <thamaesamuel@gmail.com>
2026-06-20 21:34:29 +0200

    updated version

--- src/Services/ProductsApi.js
+++ src/Services/ProductsApi.js
@@ -1,57 +1,54 @@
 import Api from "./api";
 
 export const GetProducts = async () => {
-    const response = await Api.get("/product");
-    return response.data;
+  const response = await Api.get("/product");
+  return response.data;
 };
 
-export const GetProduct  = async (barcode) => {
-    const response = await Api.get(`/product/${barcode}`);
-    return response.data;
-};
-export const GetOneProduct  = async (id) => {
-    const response = await Api.get(`/product/${id}`);
-    return response.data;
+export const GetProduct = async (barcode) => {
+  const response = await Api.get(`/product/${barcode}`);
+  return response.data;
 };
 
+export const GetOneProduct = async (id) => {
+  const response = await Api.get(`/product/${id}`);
+  return response.data;
+};
 
 export const GenerateBarcode = async (barcode) => {
   const response = await Api.get(
-    `/product/barcode/${barcode}`
+    `/product/barcode/${encodeURIComponent(barcode)}`
   );
 
   return response.data;
 };
 
-export const CreateProduct  = async (product) => {
-    const response = await Api.post("/product", product);
-    return response.data;
+export const CreateProduct = async (product) => {
+  const response = await Api.post("/product", product);
+  return response.data;
 };
 
-export const UpdateProduct  = async (product) => {
-  
-    try {
-        const response = await Api.put(`/product/${product.ProductName}`, product);
-        
-        return response.data;
-    } catch (err) {
-        console.log(err)
-    }
+export const UpdateProduct = async (product) => {
+  const response = await Api.put(
+    `/product/${product.Barcode}`,
+    product
+  );
 
+  return response.data;
 };
 
-export const DeleteProduct = async (id) => {
-  
-    const response = await Api.delete(`/product/${id}`);
-    return response.data;
+export const DeleteProduct = async (barcode) => {
+  const response = await Api.delete(
+    `/product/${encodeURIComponent(barcode)}`
+  );
+
+  return response.data;
 };
 
-export const ScanProduct = async (barcode)=>{
-    try{
-        const response = await  Api.get(`/barcode/${barcode}`);
-        return response.data
-    }catch(err){
-    console.log(err.message)
-    }
-    
+export const ScanProduct = async (barcode) => {
+  const response = await Api.get(
+    `/product/scan/${encodeURIComponent(barcode)}`
+  );
+
+  return response.data;
 };
